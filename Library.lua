17bcb3bb7ff53ff2ddb1e484d68e4d056c0eadf1
NauXLit <canyrun8053@gmail.com>
2025-06-13 14:02:30 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -5717,7 +5717,7 @@ function Library:CreateWindow(WindowInfo)
         task.spawn(Library.Toggle)
     end
 
-    if Library.IsMobile then
+  --[[  if Library.IsMobile then
         local ToggleButton = Library:AddDraggableButton("Toggle", function()
             Library:Toggle()
         end)
@@ -5736,7 +5736,7 @@ function Library:CreateWindow(WindowInfo)
         else
             LockButton.Button.Position = UDim2.fromOffset(6, 46)
         end
-    end
+    end]]
 
     --// Execution \\--
     SearchBox:GetPropertyChangedSignal("Text"):Connect(function()
